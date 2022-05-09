52f2041fb95373499ec0240fb8e26db80aa22b94
ry10an23 <kawaguchi.r823@gmail.com>
2022-05-09 00:26:35 -0700

    define the login infomation username and password

--- login.php
+++ login.php
@@ -1,5 +1,11 @@
 <?php
+    include('config.php');
 
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){ 
+        $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    }
 ?>
 
 <!DOCTYPE html>
